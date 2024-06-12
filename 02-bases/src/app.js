943591dd09cbf798602561aa51ff543bcd3fea63
Fabio <fabioescobarardila@hotmail.com>
2024-06-12 13:58:28 -0500

    Primer commit curso NodeJS con Fernando Herrera

---
+++ 02-bases/src/app.js
@@ -0,0 +1,4 @@
+
+const templateReceived = require('./js-foundation/01-template');
+
+console.log( templateReceived );
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,16 @@
+
+const template = `
+<div>
+    <h1>Hi, {{name}}</h1>
+    <p>Thank you for your order.</p>
+</div>
+`;
+
+
+console.log('Inicio');
+console.log(template);
+console.log('Final');
+
+module.exports = {
+    template
+}
