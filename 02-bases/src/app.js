87ad28929c7d63555c91032211885001f4cf1de6
Fabio <fabioescobarardila@hotmail.com>
2024-06-13 23:16:59 -0500

    Implementando libreria winston con patron adaptador

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,8 +1,22 @@
-const { uuid, getAge } = require("./plugins")
+// const { uuid, getAge } = require("./plugins")
 
-const promiseGetPoke = require("./js-foundation/06-promises");
+// const promises = require("./js-foundation/06-promises");
+const { buildLogger } = require("./plugins")
 
-promiseGetPoke(2)
-.then( pokemon => console.log(pokemon.name))
-.catch( error => console.log("Intenta de nuevo"))
-.finally(() => console.log('Finalizado'))
+
+const loggerPlugin = buildLogger('app.js');
+
+loggerPlugin.log("hola mundo");
+loggerPlugin.error("esto es malo");
+
+
+
+
+
+
+
+
+// promiseGetPoke(2)
+// .then( pokemon => console.log(pokemon.name))
+// .catch( error => console.log("Intenta de nuevo"))
+// .finally(() => console.log('Finalizado'))
--- 02-bases/src/plugins/index.js
+++ 02-bases/src/plugins/index.js
@@ -1,9 +1,11 @@
 const { uuid } = require('../plugins/uuid.plugin');
 const { getAge } = require('../plugins/getAge.plugin');
 const { httpPlugin } = require('./httpClient.plugin');
+const buildLogger = require('../plugins/logger.plugin')
 
 module.exports = {
     uuid,
     getAge,
     httpPlugin,
+    buildLogger
 }
