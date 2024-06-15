8e05f6c6030e07b020bf931af57b88ba7590fd81
Fabio <fabioescobarardila@hotmail.com>
2024-06-15 15:53:42 -0500

    Inicio: Migrando proyecto 02-base a typescript

--- 03-typescript/dist/app.js
+++ 03-typescript/dist/app.js
@@ -1,24 +1,6 @@
 "use strict";
 var _a;
-const heroes = [
-    {
-        id: 1,
-        name: "Spiderman",
-        owner: "Marvel"
-    },
-    {
-        id: 2,
-        name: "Ironman",
-        owner: "Marvel"
-    },
-    {
-        id: 3,
-        name: "Batman",
-        owner: "DC"
-    },
-];
-const findHeroById = (id) => {
-    return heroes.find(heroes => heroes.id == id);
-};
-const hero = findHeroById(2);
+Object.defineProperty(exports, "__esModule", { value: true });
+const heroes_service_1 = require("./services/heroes.service");
+const hero = (0, heroes_service_1.findHeroById)(4);
 console.log((_a = hero === null || hero === void 0 ? void 0 : hero.name) !== null && _a !== void 0 ? _a : 'Heroe no encontrado!!');
--- 03-typescript/src/app.ts
+++ 03-typescript/src/app.ts
@@ -1,26 +1,5 @@
+import { findHeroById } from './services/heroes.service'
 
-const heroes = [
-    {
-        id:1,
-        name:"Spiderman",
-        owner: "Marvel"
-    },
-    {
-        id:2,
-        name:"Ironman",
-        owner: "Marvel"
-    }, 
-    {
-        id:3,
-        name:"Batman",
-        owner: "DC"
-    },  
-]
-
-const findHeroById = ( id: number ) => {
-    return heroes.find( heroes => heroes.id == id );
-}
-
-const hero = findHeroById(2);
+const hero = findHeroById(4);
 
 console.log(hero?.name ?? 'Heroe no encontrado!!');
