71e1082b2a45b0b0cc9cf192b1e7a92ec8ec5e14
Fabio <fabioescobarardila@hotmail.com>
2024-06-14 23:45:38 -0500

    Instalando tsc y ts-node

---
+++ 03-typescript/src/app.ts
@@ -0,0 +1,26 @@
+
+const heroes = [
+    {
+        id:1,
+        name:"Spiderman",
+        owner: "Marvel"
+    },
+    {
+        id:2,
+        name:"Ironman",
+        owner: "Marvel"
+    }, 
+    {
+        id:3,
+        name:"Batman",
+        owner: "DC"
+    },  
+]
+
+const findHeroById = ( id: number ) => {
+    return heroes.find( heroes => heroes.id == id );
+}
+
+const hero = findHeroById(5);
+
+console.log(hero?.name ?? 'Heroe no encontrado');
